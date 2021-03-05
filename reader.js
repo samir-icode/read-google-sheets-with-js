b395df330f48bd7a0b16b1855520ce2b97fdf561
samir-icode <samir_icode@hotmail.com>
2021-03-05 13:46:05 +0400

    Create reader.js
    This file can be linked as external file or the script can be taken and used directly inside html code

---
+++ reader.js
@@ -0,0 +1,15 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+
+<script>
+//Published file goes here in csv format.
+    $.ajax("url_goes_here").done(
+        function(result){
+            var data = [];
+                var rows = result.split("\n");
+                for (var i = 0; i < rows.length; i++) {
+                    var cells = rows[i].split(",");
+                        //Keep html appednig in one line, it can give error to seperating lines
+                        $("#main").append("<div class='col-sm-4 col-xs-3' id='demo' style='text-align:right; color:#453bcf;'><h4>" + cells[0]+ "<span></span></h4></div>");
+                }
+        });
+</script>
